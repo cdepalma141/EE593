b0418718798b2d8590d6b14576218e18a2563ad9
Connor DePalma <cdepalma770@gmail.com>
2018-09-15 20:01:59 -0400

    hu

--- Session1/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ Session1/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/primes.dir/DependInfo.cmake"
+  "CMakeFiles/main.dir/DependInfo.cmake"
   "CMakeFiles/Session1.dir/DependInfo.cmake"
   )
--- Session1/main.cpp
+++ Session1/main.cpp
@@ -1,8 +1,100 @@
 #include <iostream>
+#include <cmath>
+#include <bitset>
 
 using namespace std;
- 
+
+int ES(int b) {
+
+    int c = int(sqrt(b));
+    bool *Prime = new bool[b + 1];
+    int count = 0;
+
+    for (int i = 1; i < b + 1; i++)
+        Prime[i] = true;
+
+//    for (int i = 0; i < b + 1; i++) {
+//        //cout << Prime[i] << "\t";
+//    }
+
+    //cout << '\n';
+
+
+    for (int i = 2; i < c + 1; i++) {
+        if (Prime[i]) {
+            for (int j = i * i; j <= b; j += i) {
+                Prime[j] = false;
+            }
+        }
+    }
+
+//    for (int i = 0; i <= b; i++) {
+//        cout << Prime[i] << "\t";
+//    }
+//
+//    cout << '\n';
+
+
+    for (int i = 0; i <= b; i++) {
+        if (Prime[i])
+            count++;
+    }
+
+    if (Prime[b])
+     return count-1;
+
+    return count;
+}
+
+int Erastothenes(int a, int b) {
+
+    int c = int(sqrt(b));
+    bool *Prime = new bool[b + 1];
+    int count = 0;
+    cout << ES(a) << '\n';
+    int count2 = ES(a);
+
+    for (int i = 1; i < b + 1; i++)
+        Prime[i] = true;
+
+//    for (int i = 0; i < b + 1; i++) {
+//        cout << Prime[i] << "\t";
+//    }
+//
+//    cout << '\n';
+
+
+    for (int i = 2; i < c + 1; i++) {
+        if (Prime[i]) {
+            for (int j = i * i; j <= b; j += i) {
+                Prime[j] = false;
+            }
+        }
+    }
+
+//    for (int i = 0; i <= b; i++) {
+//        cout << Prime[i] << "\t";
+//    }
+//
+//    cout << '\n';
+
+
+    for (int i = 0; i <= b; i++) {
+        if (Prime[i])
+            count++;
+    }
+    return count - count2;
+}
+
+
 
 int main() {
- 
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    cout << Erastothenes(a,b);
+    cout << "\n\n";
+
+
 }
