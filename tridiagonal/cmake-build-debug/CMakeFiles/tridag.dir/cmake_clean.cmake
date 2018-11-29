af0805d355ff239c05d913c6f346e07d71afa6a7
Connor DePalma <connordepalma@Connors-MacBook-Pro.local>
2018-11-28 22:53:02 -0500

    tridiagonal hw

---
+++ tridiagonal/cmake-build-debug/CMakeFiles/tridag.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tridag.dir/tridag.cpp.o"
+  "tridag.pdb"
+  "tridag"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tridag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tridiagonal/cmake-build-debug/CMakeFiles/tridiagonal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tridiagonal.dir/main.cpp.o"
+  "tridiagonal.pdb"
+  "tridiagonal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tridiagonal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
